bfa6dd3193c83381ec1c2691f845f62ea6d6d89c
Aliaksei_Shvants <Aliaksei_Shvants@epam.com>
2020-07-15 18:53:06 +0300

    my family

--- lib/widget/appbar/account/family_screen.dart
+++ lib/widget/appbar/account/family_screen.dart
@@ -5,10 +5,19 @@ import 'package:AibolitFlutter/utils/data.dart';
 import 'package:AibolitFlutter/widget/appbar/account/family_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
-class FamilyScreen extends StatelessWidget {
+class FamilyScreen extends StatefulWidget {
+  @override
+  _FamilyScreenState createState() => _FamilyScreenState();
+}
+
+class _FamilyScreenState extends State<FamilyScreen> {
   final List<User> family = Data.owner.family;
+
   final String _appBarTitle = 'Моя семья';
+
+  var forceReload = false;
 
   @override
   Widget build(BuildContext context) {
@@ -16,27 +25,58 @@ class FamilyScreen extends StatelessWidget {
       appBar: AppWidgets.getAppBar(context, _appBarTitle),
       body: SafeArea(
         child: Container(
-//          width: double.infinity,
           color: AppColors.grey200,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Stack(
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    AppWidgets.getTextHeader(_appBarTitle),
-                    ..._getFamily(family),
-                  ],
-                ),
-              ],
-            ),
-          ),
+          child: FutureBuilder<void>(
+              future: forceReload ? _reload() : null,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        AppWidgets.getTextHeader(_appBarTitle),
+                        ..._getFamily(family),
+                      ],
+                    ),
+                  );
+                }
+              }),
         ),
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          FloatingActionButton(
+            mini: true,
+            child: Icon(
+              MaterialCommunityIcons.reload,
+              color: Colors.white,
+            ),
+            backgroundColor: AppColors.green,
+            onPressed: () => setState(() {
+              forceReload = true;
+            }),
+          ),
+          FloatingActionButton(
+            mini: true,
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            backgroundColor: AppColors.green,
+          ),
+        ],
       ),
     );
   }
 
   List<FamilyItem> _getFamily(List<User> list) =>
       List.generate(list.length, (index) => FamilyItem(list[index]));
+
+  Future<void> _reload() async {
+    await Future.delayed(Duration(seconds: 3));
+  }
 }
